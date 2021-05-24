d272578477c653c71ff4deec0728c64ee7151c7f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-05-24 12:04:26 +0200

    enhance click input

--- src/views/landing.ts
+++ src/views/landing.ts
@@ -3,7 +3,7 @@ import { hand } from './hand'
 
 export const landing = div('landing align-middle flex flex-col items-center justify-center')
 
-const title = h1('title text-5xl font-bold sm:text-7xl mb-4 text-white backdrop-filter backdrop-grayscale backdrop-opacity-40', 'Finga !')
+const title = h1('title text-5xl font-bold sm:text-7xl mb-20 text-white backdrop-filter backdrop-grayscale backdrop-opacity-40', 'Finga !')
 
 landing.append(title)
 landing.append(hand)
